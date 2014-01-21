0c1e560b46bb562e93082cf061e651f9aa7e0e1a
Edward Shaw <edward.shaw@worc.ox.ac.uk>
2014-01-21 22:56:25 +0000

    Smarter caching

--- app/app.rb
+++ app/app.rb
@@ -12,19 +12,22 @@ module Calmanac
     enable :sessions
     enable :caching
 
-    uri = URI.parse(ENV["REDISCLOUD_URL"])
-    puts uri
+    redis = URI.parse(ENV["REDISCLOUD_URL"])
     set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(
-                                                     :host => uri.host,
-                                                     :port => uri.port,
-                                                     :password => uri.password
+                                                     :host => redis.host,
+                                                     :port => redis.port,
+                                                     :password => redis.password
                                                  ))
     get '/' do
-      "Hello! :) You can find the lecture time table <a href='./lectures.ics'>here.</a> "
+      "Hello! :) You can find the lecture time table <a href='./lectures.ics'>here.</a>."
     end
 
-    def filter_calendar(ics, courses)
-      components = RiCal.parse_string(ics)
+    def filter_calendar(url, courses)
+
+      components = cache( "parsed-cal-#{url}", :expires_in => 60*60*6) do
+        ics = Net::HTTP.get(URI.parse(url))
+        RiCal.parse_string(ics)
+      end
 
       if courses then
         components[0].events.select! do |c|
@@ -34,31 +37,26 @@ module Calmanac
       components
     end
 
-    def get_ics(url)
-      cache( "base-cal-#{url}", :expires_in => 60*60*6 ) do
-          Net::HTTP.get(URI.parse('http://www.cs.ox.ac.uk/feeds/Timetable-Lecture.ics'))
-      end
+
+    def construct_calendar(cal_uri)
+      components = filter_calendar(cal_uri, params["courses"])
+      content_type 'text/calendar'
+      components
     end
 
     get '/lectures.ics' do
-      ics = get_ics('http://www.cs.ox.ac.uk/feeds/Timetable-Lecture.ics')
-      components = filter_calendar(ics, params["courses"])
-      content_type 'text/calendar'
-      components[0].to_s
+      cal_uri = 'http://www.cs.ox.ac.uk/feeds/Timetable-Lecture.ics'
+      construct_calendar(cal_uri).to_s
     end
 
     get '/practicals.ics' do
-      ics = get_ics('http://www.cs.ox.ac.uk/feeds/Timetable-Practical.ics')
-      components = filter_calendar(ics, params["courses"])
-      content_type 'text/calendar'
-      components[0].to_s
+      cal_uri = 'http://www.cs.ox.ac.uk/feeds/Timetable-Practical.ics'
+      construct_calendar(cal_uri).to_s
     end
 
     get '/classes.ics' do
-      ics = get_ics('http://www.cs.ox.ac.uk/feeds/Timetable-Class.ics')
-      components = filter_calendar(ics, params["courses"])
-      content_type 'text/calendar'
-      components[0].to_s
+      cal_uri = 'http://www.cs.ox.ac.uk/feeds/Timetable-Class.ics'
+      construct_calendar(cal_uri).to_s
     end
 
   end
